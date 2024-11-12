bce31210b31119105892ee035f029d1a0decd4d3
stephanp123 <Spopescu5940@conestogac.on.ca>
2024-11-12 16:39:47 -0500

    Logs is Complete (with .txt file)

--- src/main/java/com/group3/FileIO.java
+++ src/main/java/com/group3/FileIO.java
@@ -3,42 +3,16 @@ package com.group3;
 import com.group3.objects.Logs;
 import com.group3.objects.Sensor;
 
+import java.io.IOException;
+import java.io.FileWriter;
 import java.io.BufferedWriter;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileIO {
 
-    private String logFilePath;
-    private BufferedWriter writeToFile;
-    private List<Logs> logEntry;
-    private String errorCatch;
 
-
-    public String operatorLogs(String filePath){
-
-        System.out.println("peepee");
-
-        return "success";
-    }
-
-    public Logs writeLogs(Logs logging){
-
-        return logging;
-
-    }
-
-    private List<Logs> formatLogs(String action, LocalDateTime timestamp, List<Sensor> sensorList) {
-
-        List<Logs> logList = new ArrayList<>();
-
-        return logList;
-    }
-
-    private String logError(String error){
-
-        return "poopoo";
-    }
 
 }
