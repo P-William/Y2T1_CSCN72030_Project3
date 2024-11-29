ec3df072574e7e399646424049e40e32d5eae11c
Brooke Cronin <brookesynchro123@gmail.com>
2024-11-28 22:20:31 -0500

    Began to make the GUI more user-friendly. Allows the user to pause and resume simulator. The sliders must always be a whole number.

--- src/main/java/com/group3/PrimaryController.java
+++ src/main/java/com/group3/PrimaryController.java
@@ -22,6 +22,7 @@ import javafx.scene.control.Label;
 
 public class PrimaryController {
 
+
     private ControlDevice coolantValve = new ControlDevice("Coolant Valve", 0d, 100d, 50d, "Percent");
     private ControlDevice coolantPump = new ControlDevice("Coolant Pump", 0d, 100d, 50d, "Percent");
     private ControlDevice controlRods = new ControlDevice("Control Rods", 0d, 100d, 75d, "Percent");
@@ -35,16 +36,41 @@ public class PrimaryController {
 
     private Simulator simulator;
 
-    @FXML
-    private Slider coolantPumpGui;
+
+
+
+
+
     @FXML
     private Slider coolantValveGui;
     @FXML
+    private Label LblCoolantValveTargetValue;
+    @FXML
+    private Label LblCoolantValveCurrentValue;
+    @FXML
+    private Slider coolantPumpGui;
+    @FXML
+    private Label LblCoolantPumpTargetValue;
+    @FXML
+    private Label LblCoolantPumpCurrentValue;
+    @FXML
     private Slider controlRodGui;
+    @FXML
+    private Label LblControlRodsTargetValue;
+    @FXML
+    private Label LblControlRodsCurrentValue;
     @FXML
     private Slider steamLevelGui;
     @FXML
+    private Label LblSteamOutputTargetValue;
+    @FXML
+    private Label LblSteamOutputCurrentValue;
+    @FXML
     public Slider pressureValveGui;
+    @FXML
+    private Label LblPressureValveTargetValue;
+    @FXML
+    private Label LblPressureValveCurrentValue;
     @FXML
     private Label state;
     @FXML
@@ -54,12 +80,11 @@ public class PrimaryController {
     private Logs logs;
 
     @FXML
-    private ToggleButton run;
+    private ToggleButton pause;
 
     @FXML
     private void initialize() {
         logs = new Logs("logs.txt");
-        run = new ToggleButton("Run");
         simulator = new Simulator();
         controls = new ArrayList<>();
         sensors = new ArrayList<>();
@@ -73,10 +98,29 @@ public class PrimaryController {
         controls.add(corePressureBlowOff);
 
         coolantValveGui.setValue(simulator.coolantValve.getCurrentValue());
+        coolantValveGui.valueProperty().addListener((observable, oldValue, newValue) -> {
+            coolantValveGui.setValue(Math.round(newValue.doubleValue()));
+        });
+
         coolantPumpGui.setValue(simulator.coolantPump.getCurrentValue());
+        coolantPumpGui.valueProperty().addListener((observable, oldValue, newValue) -> {
+            coolantPumpGui.setValue(Math.round(newValue.doubleValue()));
+        });
+
         controlRodGui.setValue(simulator.controlRods.getCurrentValue());
+        controlRodGui.valueProperty().addListener((observable, oldValue, newValue) -> {
+            controlRodGui.setValue(Math.round(newValue.doubleValue()));
+        });
+
         steamLevelGui.setValue(simulator.steamRate.getCurrentValue());
+        steamLevelGui.valueProperty().addListener((observable, oldValue, newValue) -> {
+            steamLevelGui.setValue(Math.round(newValue.doubleValue()));
+        });
+
         pressureValveGui.setValue(simulator.corePressureBlowOff.getCurrentValue());
+        pressureValveGui.valueProperty().addListener((observable, oldValue, newValue) -> {
+            pressureValveGui.setValue(Math.round(newValue.doubleValue()));
+        });
 
        //this is the thread that is run every 0.25 seconds
         timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
@@ -88,29 +132,38 @@ public class PrimaryController {
             }
         }));
         timeline.setCycleCount(Timeline.INDEFINITE);//set the timeline to loop forever/until the window is closed
-    }
 
-    @FXML
-    private void onRunButtonClicked() throws IOException, InterruptedException {
-        if (!run.isSelected()) {
-            System.out.println("Turning Reactor On!");
-            timeline.play();
-            run.setSelected(true);
-        }
-        else if(run.isSelected()) {
-            System.out.println("Turning reactor Off!");
-            timeline.pause();
-            run.setSelected(false);
-        }
+        timeline.play();
+        pause.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                System.out.println("Resuming Reactor");
+                timeline.play();
+                pause.setText("Pause");
+            } else {
+                System.out.println("Pausing Reactor");
+                timeline.pause();
+                pause.setText("Resume");
+            }
+        });
     }
 
     private void SimulateReactor() throws IOException {
         double increment = 1d;
         coolantValve.setTargetValue(coolantValveGui.getValue());
+        LblCoolantValveTargetValue.setText(Double.toString(coolantValve.getTargetValue()));
+        LblCoolantValveCurrentValue.setText(Double.toString(coolantValve.getCurrentValue()));
         coolantPump.setTargetValue(coolantPumpGui.getValue());
+        LblCoolantPumpTargetValue.setText(Double.toString(coolantPump.getTargetValue()));
+        LblCoolantPumpCurrentValue.setText(Double.toString(coolantPump.getCurrentValue()));
         controlRods.setTargetValue(controlRodGui.getValue());
+        LblControlRodsTargetValue.setText(Double.toString(controlRods.getTargetValue()));
+        LblControlRodsCurrentValue.setText(Double.toString(controlRods.getCurrentValue()));
         steamRate.setTargetValue(steamLevelGui.getValue());
+        LblSteamOutputTargetValue.setText(Double.toString(steamRate.getTargetValue()));
+        LblSteamOutputCurrentValue.setText(Double.toString(steamRate.getCurrentValue()));
         corePressureBlowOff.setTargetValue(pressureValveGui.getValue());
+        LblPressureValveTargetValue.setText(Double.toString(corePressureBlowOff.getTargetValue()));
+        LblPressureValveCurrentValue.setText(Double.toString(corePressureBlowOff.getCurrentValue()));
 
         for(ControlDevice controlDevice : controls) {
 
